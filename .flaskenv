8e18dfe070c90c4ca2e51b9d02c96bcf812d4928
Jeremy Milledge <jez.milledge@gmail.com>
2020-01-02 23:11:24 +1100

    template added

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=florsk.py
+FLASK_DEBUG=1
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return 'hello world'
+  user = {'username': 'Jez'}
+  title= 'Home'
+  posts = [
+    {
+      'author': {'username': 'Jez'},
+      'body': 'test jez article'
+    },
+    {
+      'author': {'username': 'Jez'},
+      'body': 'yet another Jez test'
+    }
+  ]
+  return render_template('index.html', title=title, user=user, posts=posts)
+  
